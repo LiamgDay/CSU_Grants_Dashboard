2af9bd5f395ee3e0f256ec7f66c31981c609c757
Liam <liamgday03@gmail.com>
2026-07-03 23:18:45 -0700

    Added Northridge. USAspending API is extremely slow currently, heavily affecting load speed.

--- campuses.py
+++ campuses.py
@@ -3,268 +3,142 @@ CSU_CAMPUSES = [
         "id": "san_luis_obispo",
         "display_name": "San Luis Obispo",
         "recipients": [
-            {
-                "name": "California Polytechnic State University",
-                "uei": "CPVFEDRAL3U6",
-            },
-            {
-                "name": "California Polytechnical University",
-                "uei": "",
-            },
-            {
-                "name": "Cal Poly Corporation",
-                "uei": "MC4RJJM9XLT5",
-            },
+            {"name": "California Polytechnic State University", "uei": "CPVFEDRAL3U6"},
+            {"name": "California Polytechnical University", "uei": ""},
+            {"name": "Cal Poly Corporation", "uei": "MC4RJJM9XLT5"},
         ],
     },
     {
         "id": "san_diego",
         "display_name": "San Diego",
         "recipients": [
-            {
-                "name": "San Diego State University Foundation",
-                "uei": "H59JKGFZKHL7",
-            },
-            {
-                "name": "San Diego State University Fou",
-                "uei": "",
-            },
-            {
-                "name": "San Diego State University",
-                "uei": "RN4HGABGRWV7",
-            },
-            {
-                "name": "SDVAFA / SDSU",
-                "uei": "",
-            },
-            {
-                "name": "SDSU Growth Partnership",
-                "uei": "",
-            },
+            {"name": "San Diego State University Foundation", "uei": "H59JKGFZKHL7"},
+            {"name": "San Diego State University Fou", "uei": ""},
+            {"name": "San Diego State University", "uei": "RN4HGABGRWV7"},
+            {"name": "SDVAFA / SDSU", "uei": ""},
+            {"name": "SDSU Growth Partnership", "uei": ""},
         ],
     },
     {
         "id": "channel_islands",
         "display_name": "Channel Islands",
         "recipients": [
-            {
-                "name": "California State University, Channel Islands Foundation",
-                "uei": "XKLPG9DAK5J9",
-            },
-            {
-                "name": "California State University Channel Islands",
-                "uei": "RN64EPNH8JC6",
-            },
+            {"name": "California State University, Channel Islands Foundation", "uei": "XKLPG9DAK5J9"},
+            {"name": "California State University Channel Islands", "uei": "RN64EPNH8JC6"},
         ],
     },
     {
         "id": "bakersfield",
         "display_name": "Bakersfield",
         "recipients": [
-            {
-                "name": "CALIFORNIA STATE UNIVERSITY, BAKERSFIELD AUXILIARY FOR SPONSORED PROGRAMS ADMINISTRATION",
-                "uei": "VJQPUC5YLK87",
-            },
-            {
-                "name": "CALIFORNIA STATE UNIVERSITY BAKERSFIELD",
-                "uei": "KJKYFMDKP1V9",
-            },
-            {
-                "name": "CALIFORNIA STATE UNIVERSITY, BAKERSFIELD",
-                "uei": "T3D6LYM21635",
-            },
-            {
-                "name": "CALIFORNIA STATE UNIVERSITY, BAKERSFIELD, FOUNDATION",
-                "uei": "NALMGRDCMCH1",
-            },
+            {"name": "California State University, Bakersfield Auxiliary for Sponsored Programs Administration", "uei": "VJQPUC5YLK87"},
+            {"name": "California State University Bakersfield", "uei": "KJKYFMDKP1V9"},
+            {"name": "California State University, Bakersfield", "uei": "T3D6LYM21635"},
+            {"name": "California State University, Bakersfield, Foundation", "uei": "NALMGRDCMCH1"},
         ],
     },
     {
         "id": "chico",
         "display_name": "Chico",
         "recipients": [
-            {
-                "name": "Chico State Enterprises",
-                "uei": "C4VMQLSU1LF4",
-            },
-            {
-                "name": "California State University, Chico",
-                "uei": "MXA6A75JEGY8",
-            },
-            {
-                "name": "California State University - Chico",
-                "uei": "",
-            },
-            {
-                "name": "CALIFORNIA STATE UNIV. CHICO RESEARCH FOUNDAT",
-                "uei": "K1FMBANT8GC4",
-            },
-            {
-                "name": "CALIFORNIA STATE UNIV. CHICO",
-                "uei": "",
-            },
+            {"name": "Chico State Enterprises", "uei": "C4VMQLSU1LF4"},
+            {"name": "California State University, Chico", "uei": "MXA6A75JEGY8"},
+            {"name": "California State University - Chico", "uei": ""},
+            {"name": "California State Univ. Chico Research Foundat", "uei": "K1FMBANT8GC4"},
+            {"name": "California State Univ. Chico", "uei": ""},
         ],
     },
     {
         "id": "dominguez_hills",
         "display_name": "Dominguez Hills",
         "recipients": [
-            {
-                "name": "CALIFORNIA STATE UNIVERSITY, DOMINGUEZ HILLS FOUNDATION",
-                "uei": "MWEPWP3T6XL5",
-            },
-            {
-                "name": "CALIFORNIA STATE UNIVERSITY, DOMINGUEZ HILLS",
-                "uei": "HMW5V5LQY617",
-            },
+            {"name": "California State University, Dominguez Hills Foundation", "uei": "MWEPWP3T6XL5"},
+            {"name": "California State University, Dominguez Hills", "uei": "HMW5V5LQY617"},
         ],
     },
     {
         "id": "east_bay",
         "display_name": "East Bay",
         "recipients": [
-            {
-                "name": "CALIFORNIA STATE UNIVERSITY, EAST BAY FOUNDATION, INC.",
-                "uei": "GEXJV1ZLVDM8",
-            },
-            {
-                "name": "CALIFORNIA STATE UNIVERSITY EAST BAY",
-                "uei": "XB4KBLWF4LA5",
-            },
+            {"name": "California State University, East Bay Foundation, Inc.", "uei": "GEXJV1ZLVDM8"},
+            {"name": "California State University East Bay", "uei": "XB4KBLWF4LA5"},
         ],
     },
     {
         "id": "fresno",
         "display_name": "Fresno",
         "recipients": [
-            {
-                "name": "CALIFORNIA STATE UNIVERSITY, FRESNO FOUNDATION",
-                "uei": "CJSRSPWTJUH7",
-            },
-            {
-                "name": "CALIFORNIA STATE UNIVERSITY, FRESNO",
-                "uei": "JLQBUB5SG3L5",
-            },
-            {
-                "name": "CALIFORNIA STATE UNIVERSITY FRESNO",
-                "uei": "GBATCBNV1SH8",
-            },
+            {"name": "California State University, Fresno Foundation", "uei": "CJSRSPWTJUH7"},
+            {"name": "California State University, Fresno", "uei": "JLQBUB5SG3L5"},
+            {"name": "California State University Fresno", "uei": "GBATCBNV1SH8"},
         ],
     },
     {
         "id": "fullerton",
         "display_name": "Fullerton",
         "recipients": [
-            {
-                "name": "CALIFORNIA STATE UNIVERSITY, FULLERTON FOUNDA",
-                "uei": "",
-            },
-            {
-                "name": "CALIFORNIA STATE UNIVERSITY, FULLERTON",
-                "uei": "Q4V7X8BCUSH3",
-            },
-            {
-                "name": "CALIFORNIA STATE UNIVERSITY -FULLERTON",
-                "uei": "H5D8ZCWMBRQ9",
-            },
-            {
-                "name": "ASSOCIATED STUDENTS, CALIFORNIA STATE UNIVERSITY, FULLERTON, INC",
-                "uei": "PRADK6JRJD98",
-            },
-            {
-                "name": "CSU FULLERTON AUXILIARY SERVICES CORPORATION",
-                "uei": "VQ5WK498QDC6",
-            },
+            {"name": "California State University, Fullerton Founda", "uei": ""},
+            {"name": "California State University, Fullerton", "uei": "Q4V7X8BCUSH3"},
+            {"name": "California State University -Fullerton", "uei": "H5D8ZCWMBRQ9"},
+            {"name": "Associated Students, California State University, Fullerton, Inc", "uei": "PRADK6JRJD98"},
+            {"name": "CSU Fullerton Auxiliary Services Corporation", "uei": "VQ5WK498QDC6"},
         ],
     },
     {
         "id": "humboldt",
         "display_name": "Humboldt",
         "recipients": [
-            {
-                "name": "CAL POLY HUMBOLDT SPONSORED PROGRAMS FOUNDATION",
-                "uei": "K1S8M8RU4FK7",
-            },
-            {
-                "name": "HUMBOLDT STATE UNIVERSITY SPONSORED PROGRAMS",
-                "uei": "",
-            },
-            {
-                "name": "HUMBOLDT STATE UNIVERSITY SPONSORED",
-                "uei": "",
-            },
-            {
-                "name": "HUMBOLDT STATE UNIVERSITY FOUNDATION",
-                "uei": "",
-            },
-            {
-                "name": "HUMBOLDT STATE UNIVERSITY",
-                "uei": "",
-            },
+            {"name": "Cal Poly Humboldt Sponsored Programs Foundation", "uei": "K1S8M8RU4FK7"},
+            {"name": "Humboldt State University Sponsored Programs", "uei": ""},
+            {"name": "Humboldt State University Sponsored", "uei": ""},
+            {"name": "Humboldt State University Foundation", "uei": ""},
+            {"name": "Humboldt State University", "uei": ""},
         ],
     },
     {
         "id": "long_beach",
         "display_name": "Long Beach",
         "recipients": [
-            {
-                "name": "CALIFORNIA STATE UNIVERSITY LONG BEACH RESEARCH FOUNDATION",
-                "uei": "P2TDH1JCJD31",
-            },
-            {
-                "name": "ASSOCIATED STUDENTS, CALIFORNIA STATE UNIVERSITY, LONG BEACH",
-                "uei": "ZMB2GX7JQMU6",
-            },
-            {
-                "name": "CALIFORNIA STATE UNIVERSITY LONG BEACH",
-                "uei": "SCLMKHNBM8M7",
-            },
+            {"name": "California State University Long Beach Research Foundation", "uei": "P2TDH1JCJD31"},
+            {"name": "Associated Students, California State University, Long Beach", "uei": "ZMB2GX7JQMU6"},
+            {"name": "California State University Long Beach", "uei": "SCLMKHNBM8M7"},
         ],
     },
     {
         "id": "los_angeles",
         "display_name": "Los Angeles",
         "recipients": [
-            {
-                "name": "CALIFORNIA STATE UNIVERSITY, LOS ANGELES",
-                "uei": "QX6MXJCSQD85",
-            },
-            {
-                "name": "CALIFORNIA STATE UNIVERSITY LOS ANGELES",
-                "uei": "JWL6V4CWPQJ1",
-            },
-            {
-                "name": "CAL STATE L.A. FEDERAL CREDIT UNION",
-                "uei": "JQMWU3LK2G93",
-            },
+            {"name": "California State University, Los Angeles", "uei": "QX6MXJCSQD85"},
+            {"name": "California State University Los Angeles", "uei": "JWL6V4CWPQJ1"},
+            {"name": "Cal State L.A. Federal Credit Union", "uei": "JQMWU3LK2G93"},
         ],
     },
     {
         "id": "maritime_academy",
         "display_name": "Maritime Academy",
         "recipients": [
-            {
-                "name": "CALIFORNIA STATE UNIVERSITY, MARITIME ACADEMY",
-                "uei": "GS1KNYFEVKF1",
-            },
+            {"name": "California State University, Maritime Academy", "uei": "GS1KNYFEVKF1"},
         ],
     },
     {
         "id": "monterey_bay",
         "display_name": "Monterey Bay",
         "recipients": [
-            {
-                "name": "UNIVERSITY CORPORATION AT MONTEREY BAY",
-                "uei": "EDSUH7KMJE91",
-            },
-            {
-                "name": "CALIFORNIA STATE UNIVERSITY OF MONTEREY BAY",
-                "uei": "ZCS6S95MGH66",
-            },
+            {"name": "University Corporation at Monterey Bay", "uei": "EDSUH7KMJE91"},
+            {"name": "California State University of Monterey Bay", "uei": "ZCS6S95MGH66"},
+        ],
+    },
+    {
+        "id": "northridge",
+        "display_name": "Northridge",
+        "recipients": [
+            {"name": "The University Corporation", "uei": "LAGNHMC58DF3"},
+            {"name": "California State University, Northridge", "uei": "JBNEPD1BJHK6"},
+            {"name": "California State University Northridge", "uei": "FNJZEE34W8V3"},
+            {"name": "Associated Students, California State University, Northridge, Inc", "uei": "U8E7BJWCYD45"},
         ],
     },
 ]
-#ASSOCIATED STUDENTS INC OF CALIFORNIA POLYTECHNIC STATE UNIVERSITY AT SAN LUIS OBISPO is in subawards
 
 
 def iter_recipients(): #flattens, changes keys, and makes an entry for each recipient name (doesn't differentiate based on similar campus)
